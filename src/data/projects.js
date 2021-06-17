0a9173ecab0263acc3f5523da8ddacfe7c09dc8d
Luis <zapataluis1@outlook.es>
2021-06-17 00:22:54 -0500

    Projects were updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,34 +1,18 @@
 export const allProjects = [
     {
-        imgLink: 'https://res.cloudinary.com/dw8xidhnh/image/upload/v1614821087/portfolio/Captura_de_pantalla_de_2021-03-03_19-20-36_owqcxv.png',
-        imgTitle: 'Heroes-img',
-        cardTitle: 'Heroes App',
-        description: 'A Single-Page Application to show information about your favorite heroes',
-        github: 'https://github.com/Lichos17/heroes-app',
-        pageLink: 'https://fervent-jepsen-314cc4.netlify.app'
+        imgLink: 'https://res.cloudinary.com/dw8xidhnh/image/upload/v1623906943/portfolio/okqxx5dotsh5thoyf5a3.png',
+        imgTitle: 'portfolio-img1',
+        cardTitle: 'Todo App',
+        description: 'Todo App using firebase, react, react-modal, react-router',
+        github: 'https://github.com/Lichos17/firebase-redux',
+        pageLink: 'https://suspicious-sinoussi-b56725.netlify.app/'
     },
     {
-        imgLink: 'https://res.cloudinary.com/dw8xidhnh/image/upload/v1614907926/portfolio/Captura_de_pantalla_de_2021-03-04_19-31-25_pyoyxr.png',
-        imgTitle: 'GiftExpertApp',
-        cardTitle: 'GiftExpertApp',
-        description: 'A simple app to search gif about anything',
-        github: 'https://github.com/Lichos17/React-GifExpertApp',
-        pageLink: 'https://naughty-mccarthy-62595f.netlify.app'
-    },
-    {
-        imgLink: 'https://res.cloudinary.com/dw8xidhnh/image/upload/v1614904780/portfolio/Captura_de_pantalla_de_2021-03-04_18-38-31_ygipla.png',
-        imgTitle: 'CocktailApp',
-        cardTitle: 'CocktailApp',
-        description: 'An app to look for differents recipes to make some drinks',
-        github: 'https://github.com/Lichos17/cocktailApp',
-        pageLink: 'https://clever-torvalds-c2a003.netlify.app'
-    },
-    {
-        imgLink: 'https://res.cloudinary.com/dw8xidhnh/image/upload/v1614905493/portfolio/Captura_de_pantalla_de_2021-03-04_18-51-00_awf5u8.png',
-        imgTitle: 'Todo',
-        cardTitle: 'TodoApp',
-        description: 'A simple TodoApp made with react',
-        github: 'https://github.com/Lichos17/TodoApp',
-        pageLink: 'https://cocky-hawking-96fc29.netlify.app'
+        imgLink: 'https://res.cloudinary.com/dw8xidhnh/image/upload/v1623906944/portfolio/qvmjjrk4yihxdepwjqik.png',
+        imgTitle: 'portfolio-img2',
+        cardTitle: 'NPM Package API',
+        description: 'A web app to show the most famous packages about React, Angular and Vue using react and babel',
+        github: 'https://github.com/Lichos17/npm-react-project',
+        pageLink: 'https://npm-portfolio.herokuapp.com/'
     }
 ];
