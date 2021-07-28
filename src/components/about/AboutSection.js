1a6dd8857c86e6740c974d1d6c68353e6396356f
Luis <zapataluis1@outlook.es>
2021-07-28 13:04:40 -0500

    portfolio updated

--- src/components/about/AboutSection.js
+++ src/components/about/AboutSection.js
@@ -4,7 +4,7 @@ export const AboutSection = () => {
 
     const handleSubmit = ( e ) => {
         e.preventDefault();
-        window.open('https://drive.google.com/file/d/1sUDVd-O7Y2QoODBYzCnJB4kx_FSM7S-V/view', "_blank");
+        window.open('https://drive.google.com/file/d/1Rb7waMZZpiN20WZ_H0jcnDvTt-fES5AY/view?usp=sharing', "_blank");
     }
 
     return (
@@ -17,7 +17,7 @@ export const AboutSection = () => {
                         <img src="https://res.cloudinary.com/dw8xidhnh/image/upload/v1614995164/portfolio/f10-min_hapoxt.jpg" alt="lichos" />
                     </div>
                     <div className="about-text flex-column">
-                        <p> I am 21 years old. Actuary student at Universidad Autonoma de Yucatán and self-taught web development student currently studying React.
+                        <p> I am 21 years old. Computer Science student at Universidad Autonoma de Yucatán and self-taught web development student currently studying React.
                             My philosophy is never stop learning and my main motivation is to learn new things day by day, searching new experiences in order to be a better professional and person in the future</p>
                         <button className="btn btn-about m-1" type="submit" onClick={ handleSubmit }>My Resume</button>
                     </div>
--- src/data/projects.js
+++ src/data/projects.js
@@ -14,5 +14,13 @@ export const allProjects = [
         description: 'A web app to show the most famous packages about React, Angular and Vue using react and babel',
         github: 'https://github.com/Lichos17/npm-react-project',
         pageLink: 'https://npm-portfolio.herokuapp.com/'
+    },
+    {
+        imgLink: 'https://res.cloudinary.com/dw8xidhnh/image/upload/v1627495340/rutvjtdr8wmkao8uwb1d.png',
+        imgTitle: 'portfolio-img3',
+        cardTitle: 'Breaking Bapp',
+        description: 'A web app using the public breaking bad api using React, React-Router',
+        github: 'https://github.com/Lichos17/breaking-api',
+        pageLink: 'https://dazzling-goldstine-6a2d0b.netlify.app'
     }
 ];
